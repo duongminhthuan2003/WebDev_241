42c4c2c8c530d063ae55ae474a96adbf76a03d2b
duongminhthuan2003 <thuan.duongminh2003@gmail.com>
2024-12-06 09:30:23 +0700

    Commit 09:30 06.12
    Update footer cho index

--- src/views/customer/index.php
+++ src/views/customer/index.php
@@ -105,7 +105,7 @@
     </div>
 </div>
 
-<div id="mobileNav" class="md:invisible hidden w-full h-[calc(100%-64px)] absolute z-30 top-16 bg-white/30 backdrop-blur">
+<div id="mobileNav" class="md:invisible hidden w-full h-[calc(100%-48px)] fixed z-30 top-12 bg-white/30 backdrop-blur">
     <div class="flex flex-col my-16 space-y-9 w-9/12 mx-auto">
         <a class="font-bold text-lg">SẢN PHẨM</a>
         <a class="font-bold text-lg">SALE-OFF</a>
@@ -232,44 +232,48 @@
     </div>
 </div>
 
-<footer class="flex flex-col justify-end w-full">
+
+<footer class="flex flex-col justify-end w-full mt-8 md:mt-0">
     <div class="flex w-full bg-white h-[400px] mb-0 bottom-0">
         <div class="my-auto top-0 bottom-0 w-full">
             <div class="flex flex-row w-9/12 mx-auto align-middle">
-                <img src="/assets/Logo_Ananas.png" class="h-7 flex my-auto top-0 bottom-0 mr-2" alt="logo">
-                <img src="/assets/logo-black.png" class="flex h-10 w-24 object-cover my-auto top-0 bottom-0" alt="logo">
+                <img src="../../../public/assets/Logo_Ananas.png" class="h-7 flex my-auto top-0 bottom-0 mr-2" alt="logo">
+                <img src="../../../public/assets/logo-black.png" class="flex h-10 w-24 object-cover my-auto top-0 bottom-0" alt="logo">
             </div>
 
-            <div class="flex flex-row w-9/12 mx-auto mt-5">
-                <div class="w-1/5 space-y-3">
-                    <p><strong>SẢN PHẨM</strong></p>
-                    <p class="text-sm text-[#888888]">Giày nam</p>
-                    <p class="text-sm text-[#888888]">Giày nữ</p>
-                    <p class="text-sm text-[#888888]">Thời trang & phụ kiện</p>
-                    <p class="text-sm text-[#888888]">Sale-off</p>
+            <div class="flex md:flex-row flex-col w-9/12 mx-auto mt-5">
+                <div class="flex flex-row md:w-3/5 w-full">
+                    <div class="w-1/3 space-y-3">
+                        <p><strong>SẢN PHẨM</strong></p>
+                        <p class="text-sm text-[#888888]">Giày nam</p>
+                        <p class="text-sm text-[#888888]">Giày nữ</p>
+                        <p class="text-sm text-[#888888]">Áo & phụ kiện</p>
+                        <p class="text-sm text-[#888888]">Sale-off</p>
+                    </div>
+
+                    <div class="w-1/3 space-y-3">
+                        <p><strong>VỀ ANANAS</strong></p>
+                        <p class="text-sm text-[#888888]">Tuyển dụng</p>
+                        <p class="text-sm text-[#888888]">Giới thiệu</p>
+                    </div>
+
+                    <div class="w-1/3 space-y-3">
+                        <p><strong>HỖ TRỢ</strong></p>
+                        <p class="text-sm text-[#888888]">FAQs</p>
+                        <p class="text-sm text-[#888888]">Bảo mật thông tin</p>
+                        <p class="text-sm text-[#888888]">Chính sách chung</p>
+                        <p class="text-sm text-[#888888]">Tra cứu đơn hàng</p>
+                    </div>
                 </div>
 
-                <div class="w-1/5 space-y-3">
-                    <p><strong>VỀ ANANAS</strong></p>
-                    <p class="text-sm text-[#888888]">Tuyển dụng</p>
-                    <p class="text-sm text-[#888888]">Giới thiệu</p>
-                </div>
 
-                <div class="w-1/5 space-y-3">
-                    <p><strong>HỖ TRỢ</strong></p>
-                    <p class="text-sm text-[#888888]">FAQs</p>
-                    <p class="text-sm text-[#888888]">Bảo mật thông tin</p>
-                    <p class="text-sm text-[#888888]">Chính sách chung</p>
-                    <p class="text-sm text-[#888888]">Tra cứu đơn hàng</p>
-                </div>
-
-                <div class="w-2/5">
+                <div class="md:w-2/5 w-full mt-8 md:mt-0">
                     <p><strong>LIÊN HỆ</strong></p>
                     <input type="text" placeholder="Email" class="border-2 w-full text-sm h-10 rounded-md mt-2 pl-3">
                     <div class="flex flex-row space-x-4 mt-3">
-                        <img src="/assets/index/fb_icon.png" alt="Facebook Icon" class="h-5 w-5 opacity-55">
-                        <img src="/assets/index/yt_icon.png" alt="Youtube Icon" class="h-5 w-5 opacity-55">
-                        <img src="/assets/index/ig_icon.png" alt="Instagram Icon" class="h-5 w-5 opacity-55">
+                        <img src="../../../public/assets/index/fb_icon.png" alt="Facebook Icon" class="h-5 w-5 opacity-55">
+                        <img src="../../../public/assets/index/yt_icon.png" alt="Youtube Icon" class="h-5 w-5 opacity-55">
+                        <img src="../../../public/assets/index/ig_icon.png" alt="Instagram Icon" class="h-5 w-5 opacity-55">
                     </div>
 
                     <div class="flex space-x-2 flex-row mt-7">
@@ -284,7 +288,7 @@
         </div>
     </div>
 
-    <div class="h-10 w-full bg-[#F15E2C]"></div>
+    <div class="h-10 md:mt-0 mt-8 w-full bg-[#F15E2C]"></div>
 </footer>
 
 <script>
