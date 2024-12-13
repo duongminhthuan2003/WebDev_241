956b3387e1a2a04e76ccaf6dc7b13b4a354feb3e
LAPTOP-6BUUF8U8\MY LE <dammaynholuyenthuyen@gmail.com>
2024-12-13 08:31:10 +0700

    thêm href

--- src/views/customer/product_list.php
+++ src/views/customer/product_list.php
@@ -35,7 +35,7 @@
     <!-- Nút bấm cho màn hình nhỏ -->
     <button 
         id="toggle-col1" 
-        class="lg:hidden bg-blue-500 text-white px-4 py-2 rounded shadow-md hover:bg-blue-600 focus:outline-none">
+        class="lg:hidden ML_button">
         Bộ lọc
     </button>
 
